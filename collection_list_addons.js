923efbdb114e8956980eaed7a28f197791996845
rricol <92322566+rricol@users.noreply.github.com>
2022-06-20 09:56:53 +0200

    color text/background modifications

--- collection_list_addons.js
+++ collection_list_addons.js
@@ -15,18 +15,20 @@ for (i = 0; i < allElements.length; i++) {
 
 // Set text color to white or black depending on the background
 
-let colors = $('.background-element');
+let bkg = $('.background-element');
+let light = '#ffffff';
+let dark = '#000000';
 
 for (i = 0; i < colors.length; i++)
 {
-	var rgbRaw = colors.eq(i).css('background-color');
+	var rgbRaw = bkg.eq(i).css('background-color');
   var rgb = rgbRaw.replace(/[^\d,]/g, '').split(',');
   if ((parseInt(rgb[0])*0.299 + parseInt(rgb[1])*0.587 + parseInt(rgb[2])*0.114) > 186) 
   {
-  	colors.eq(i).find('.text-element').css('color', '#000000');
+  	colors.eq(i).find('.text-element').css('color', dark);
   } 
   else 
   { 
-    colors.eq(i).find('.text-element').css('color', '#ffffff')
+    colors.eq(i).find('.text-element').css('color', light)
   };
 }
